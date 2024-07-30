6c6eb962738475fd657dc77f668ca18e819d1cbc
Helio Frota <00hf11@gmail.com>
2024-07-30 17:42:48 -0300

    it builds but is still weirdo

--- README.md
+++ README.md
@@ -1,6 +1,32 @@
 # plrust-lib-example
 
+Install required pgrx specific version
+
 ```
 cargo install --version 0.11.0 --locked cargo-pgrx
 ```
 
+Set PGRX_HOME to avoid cargo build error
+
+```
+export PGRX_HOME="${HOME}/.cargo/"
+```
+
+> fish
+
+```
+set -x PGRX_HOME $HOME/.cargo/
+
+```
+
+Run pgrx init 
+
+```
+cargo pgrx init --pg16 ~/Desktop/tc/trustify/.trustify/postgres/16.3.0/bin/pg_config
+```
+
+Build
+
+```
+cargo build
+```
--- src/hello.rs
+++ src/hello.rs
@@ -1,1 +1,26 @@
-fn foo() {}
+#[cfg(any(test, feature = "pg_test"))]
+#[pgrx::pg_schema]
+mod tests {
+    use pgrx::prelude::*;
+
+    #[pg_test]
+    #[search_path(@extschema@)]
+    fn plrust_basic() -> spi::Result<()> {
+        let definition = r#"
+            CREATE FUNCTION get_one() RETURNS INT
+                LANGUAGE PLRUST AS
+            $$
+                Ok(Some(1))
+            $$;
+        "#;
+        Spi::run(definition)?;
+
+        let result = Spi::get_one::<i64>(
+            r#"
+            SELECT get_one($1);
+        "#);
+        assert_eq!(result, Ok(Some(())));
+        Ok(())
+    }
+
+}
